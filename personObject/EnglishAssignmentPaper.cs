462d95aef029a2ec5af7a04e28773b35f8aab076
Awet Fessha <awetkifle94@gmail.com>
2018-10-10 10:17:51 +0100

    implement interface properties

--- personObject/EnglishAssignmentPaper.cs
+++ personObject/EnglishAssignmentPaper.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace personObject
 {
-    class EnglishAssignmentPaper
+    class EnglishAssignmentPaper: IScore
     {
         public string title { get; set; }
         public int minmumWordCount { get; set; }
         public string paperText { get; set; }
+
+        //Implementing the properties of the interface
+       public float grade { get; set; }
+       public float maximum { get; set; }
     }
 }
