61c67347193430849a9ca59822b3e5d6e5b7cb29
Awet Fessha <awetkifle94@gmail.com>
2018-10-10 09:53:32 +0100

    add more classes

---
+++ personObject/EnglishAssignmentPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personObject
+{
+    class EnglishAssignmentPaper
+    {
+        public string title { get; set; }
+        public int minmumWordCount { get; set; }
+        public string paperText { get; set; }
+    }
+}
---
+++ personObject/ProgramingAssignmentPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personObject
+{
+    class ProgramingAssignmentPaper
+    {
+        public string problemDescription { get; set; }
+        public string materials { get; set; }
+        public string solution { get; set; }
+    }
+}
