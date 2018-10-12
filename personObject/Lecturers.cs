e5108d07d03020a56d6f66c62035497e35f2190d
Awet Fessha <awetkifle94@gmail.com>
2018-10-12 10:36:37 +0100

    override virtual method in lecturer

--- personObject/Lecturers.cs
+++ personObject/Lecturers.cs
@@ -12,6 +12,17 @@ namespace personObject
         {
             return 0.0f;
         }
-        
+
+        public override string sendMessage(string message)
+        {
+            var original = base.sendMessage(message);
+            var sb = new StringBuilder(original);
+            // Let's say that our legal department at our school has gotten involved and we need to add some sort of disclaimer to all messages sent to students.
+            sb.AppendLine("");
+            sb.AppendLine("This message is private and confidential");
+            return sb.ToString();
+        }
+
+
     }
 }
