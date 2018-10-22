8f2413dc5031c025279f295eaece42ce478d345f
Awet Fessha <awetkifle94@gmail.com>
2018-10-23 00:48:46 +0100

    add new class and implement Totalgrade

---
+++ personObject/TotalgradeUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personObject
+{
+    class TotalgradeUtility
+    {
+        private static float totalgrade;
+        // let's add a static method that takes parameters which implement the interface.
+        // It can be anything that implement the interface 
+        // The static method computes the total grade of all  assignments
+
+        public static float TotalGrade(IScore EnglishAssgnimentpaper, IScore ProgrammingAssignmentPaper)
+        {
+
+            if (EnglishAssgnimentpaper != null && ProgrammingAssignmentPaper != null)
+            {
+
+                totalgrade = (EnglishAssgnimentpaper.grade + ProgrammingAssignmentPaper.grade) / 2;
+
+            }
+            return totalgrade;
+
+        }
+
+    }
+}
